4c79eca618988a75d2f900b7ca349558b5b9cd29
Chrisys-20 <chrisysdata@gmail.com>
2023-08-28 21:00:11 +0100

    update newslettter controller and other

--- app/Http/Controllers/Back/Article/CreatController.php
+++ app/Http/Controllers/Back/Article/CreatController.php
@@ -37,7 +37,7 @@ class CreatController extends Controller
             $find_ref=DB::select('select * from articles where ref="'.$ref.'"');
         }while (!empty($find_ref));
 
-        $image_path = $ref.'.'.$request->file('image')->getClientOriginalExtension();
+        $image_path = "images/article/image_".$ref.'.'.$request->file('image')->getClientOriginalExtension();
         $request->file('image')->move(public_path(),$image_path);
         
         //die(var_dump('yes'));
