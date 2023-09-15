2c387289cfd11e86c8dc13719c58b9dee4cfef45
Chrisys-20 <chrisysdata@gmail.com>
2023-09-15 20:29:42 +0100

    image articles2

--- resources/views/front/articles/article_details.blade.php
+++ resources/views/front/articles/article_details.blade.php
@@ -206,10 +206,9 @@
                                             @foreach($allArticle as $myListe)
                                             <div class="binduz-er-video-post binduz-er-recently-viewed-item">
                                                 <div class="binduz-er-latest-news-item">
-                                                    <div class="binduz-er-thumb">
-                                                        <img src="assets/images/editors-pack-thumb-1.jpg" width="299px" height="199px" alt="">
-                                                        <!-- <img src="{{$myListe->image}}" width="299px" height="199px" alt=""> -->
-                                                    </div>
+                                                    <!-- <div class="binduz-er-thumb">
+                                                        <img src="{{$myListe->image}}" width="299px" height="199px" alt="">
+                                                    </div> -->
                                                     <div class="binduz-er-content">
                                                         <div class="binduz-er-meta-item">
                                                             <div class="binduz-er-meta-categories">
